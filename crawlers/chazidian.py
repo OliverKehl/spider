bf4fcfab904c846e689a080114962d5b8872fc0c
kehl <kangjihua_home@126.com>
2015-01-13 00:06:40 +0800

    vocab and crawler

--- crawlers/chazidian.py
+++ crawlers/chazidian.py
@@ -4,8 +4,16 @@ import config
 def crawl(file):
     f = open(file,'r')
     for line in f.readlines():
-        pass
-    
+        url = 'http://www.chazidian.com/ci/?q='+line
+        req = urllib2.urlopen(url)
+        response = req.read()
+        
+        #page jump
+        offset =  response.find('title=\"'+ line +'\">')
+        target_url = response[(offset-65):(offset-2)]
+        req = urllib2.urlopen(target_url)
+        response = req.read()
+        print response
 if __name__=='__main__':
     print config.sqlalchemy_zidian_url
         
